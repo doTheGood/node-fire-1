169ca2a33aa89d97b3143d16592911a22c3ef690
Diogo Novaes <diogonovaes@gmail.com>
2021-12-30 17:52:29 +0100

    step one

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/courses', (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
-});
+const customers = [];
 
-app.post('/courses', (request, response) => {
-  const body = request.body;
-  console.log(body);
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
 
-app.put('/courses/:id', (request, response) => {
-  const { id } = request.params;
-  console.log(id);
-  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
+  const id = uuidv4();
 
-app.patch('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
-});
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
 
-app.delete('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 4']);
+  return response.status(201).send();
 });
 
 app.listen(3333);
